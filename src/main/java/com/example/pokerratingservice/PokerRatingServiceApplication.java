a3510a071e10b3fb4e76a1cc3d9c39669b2af340
AntonVasilevsky <q3antonio@gmail.com>
2023-10-02 13:23:55 +0300

    first commit

---
+++ src/main/java/com/example/pokerratingservice/PokerRatingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.pokerratingservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PokerRatingServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PokerRatingServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/pokerratingservice/PokerRatingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pokerratingservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokerRatingServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
