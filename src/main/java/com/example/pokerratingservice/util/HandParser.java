74070f9f1b767972f886f8fba540435047ce72df
AntonVasilevsky <q3antonio@gmail.com>
2023-10-03 15:45:00 +0300

    "added HandParser interface"

---
+++ src/main/java/com/example/pokerratingservice/util/HandParser.java
@@ -0,0 +1,44 @@
+package com.example.pokerratingservice.util;
+
+import com.example.pokerratingservice.Model.GameType;
+import com.example.pokerratingservice.Model.MaxPlayers;
+import com.example.pokerratingservice.Model.Player;
+
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public interface HandParser {
+    static String getStringByRegex(String line, String regex, int matcherGroupIndex) {
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(line);
+        matcher.find();
+        return matcher.group(matcherGroupIndex);
+    }
+
+    static String getStringByRegex(String line, String regex) {
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(line);
+        matcher.find(); //TODO отловить ошибки
+        return matcher.group();
+    }
+    void readFile(String path) throws IOException;
+
+
+    String getPlayerNameFromLine(String line);
+
+    MaxPlayers getMaxPLayersFromLine(String line);
+
+    String getTableNameFromLine(String line);
+
+    LocalDateTime getDateValueFromLine(String line);
+
+    double getBigBlindValueFromLine(String line);
+    double getHeroPutMoneyInPotFromLine(String line);
+
+    long getHandIdValueFromLine(String line);
+
+    GameType getGameTypeFromLine(String line);
+}
